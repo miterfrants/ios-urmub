de725dc8fec0b88ece86013e96704a292a4c6a79
miterfrants <po-hsianghunag@Po-HsiangtekiMacBook-Air.local>
2013-12-13 16:55:20 +0800

    Initial Commit

---
+++ URMUB/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  URMUB
+//
+//  Created by Po-Hsiang Hunag on 2013/12/13.
+//  Copyright (c) 2013年 Po-Hsiang Hunag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ URMUB/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  URMUB
+//
+//  Created by Po-Hsiang Hunag on 2013/12/13.
+//  Copyright (c) 2013年 Po-Hsiang Hunag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
